ef679d09e6cd05f8bceea2b9a8567dcf2382402b
MatiasHermidaUTN <83124794+MatiasHermidaUTN@users.noreply.github.com>
2022-06-27 19:50:56 -0300

    Agrego respuestas de parte 2

--- TP-MuerteEnLaMansionDreadbury.pl
+++ TP-MuerteEnLaMansionDreadbury.pl
@@ -12,13 +12,43 @@ odiaA(charles,Persona):-
 odiaA(tiaAgatha,Persona):-
     viveEnDreadbury(Persona),
     Persona \= mayordomo.
+odiaA(mayordomo,Persona):-
+    odiaA(tiaAgatha,Persona).
 
 esMasRicoQue(Persona,tiaAgatha):-
     viveEnDreadbury(Persona),
     not(odiaA(mayordomo,Persona)).
 
-odiaA(mayordomo,Persona):-
-    odiaA(tiaAgatha,Persona).
 
-% Consulta:  1 ?- quienMata(Asesino,tiaAgatha).
+% Consulta:   ?- quienMata(Asesino,tiaAgatha).
 % Respuesta: Asesino = mayordomo.
+
+%2.1 - Si existe alguien que odie a milhouse.
+% Consulta:   ?- odiaA(_,milhouse).
+% Respuesta:    false.
+
+%2.2 - A quién odia charles.
+% Consulta:   ?- odiaA(charles,Odiado).
+% Respuesta:    Odiado = mayordomo.
+
+%2.3 - El nombre de quien odia a tía Ágatha.
+% Consulta:   ?- odiaA(Odiador,tiaAgatha).
+% Respuesta:    Odiador = tiaAgatha ;
+%               Odiador = mayordomo.
+
+%2.4 - Todos los odiadores y sus odiados.
+% Consulta:   ?- odiaA(Odiador,Odiado).
+/*Resultados:   Odiador = charles,
+                Odiado = mayordomo ;
+                Odiador = Odiado, Odiado = tiaAgatha ;
+                Odiador = tiaAgatha,
+                Odiado = charles ;
+                Odiador = mayordomo,
+                Odiado = tiaAgatha ;
+                Odiador = mayordomo,
+                Odiado = charles.  
+*/
+
+%2.5 - Si es cierto que el mayordomo odia a alguien.
+% Consulta:   ?- odiaA(mayordomo,_).
+% Respuesta:    true.
